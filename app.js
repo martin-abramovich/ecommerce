921a605937b4707361b6335aae3711ece24aab4c
Lautaro Lopez <lauticlopez@gmail.com>
2023-06-18 16:49:13 -0300

    Carrito agregado sin detalles

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const publicPaht = path.resolve(__dirname, './public');
+const publicPaht = path.resolve(__dirname, "./public");
 app.use(express.static(publicPaht));
 
 app.listen(4000, () => {
-    console.log('Servidor corriendo en puerto 4000');
+  console.log("Servidor corriendo en puerto 4000");
 });
 
-app.get('/', (req,res) => {
-res.sendFile(path.resolve(__dirname, './views/index.html'));})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
 
-app.get('/registro', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/registro.html'));})
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
 
-app.get('/detalle', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/detalle.html'));})
+app.get("/detalle", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/detalle.html"));
+});
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+});
