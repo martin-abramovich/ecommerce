ee68c2c9935c68b2c10ec5eed34dcd4f30591337
Martin Abramovich <abramovich.martin@gmail.com>
2023-07-18 23:18:22 -0300

    edicion

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -14,7 +14,20 @@ const controlador = {
     },
 
     detalle: (req, res) => {
-        res.render('products/detalle');
+        /* leo un json y lo parseo */
+        const productos = JSON.parse(fs.readFileSync(productosPath, "utf-8"));
+        /* busco juego en base al req.params si existe renderizo la vista con el juego, sino un error */
+        const producto = productos.find((producto) => producto.id == req.params.id);
+        if (producto) {
+            res.render("products/detalle", { producto });
+        } else {
+            res.send(`
+                    <div style="text-align: center; padding-top:30px">
+                    <h1>El producto no existe</h1>
+                    <img style="width:40%;" src="/img/default-game.jpg">
+                    </div>
+                    `);
+            }
     },
 
     crear: (req, res) => {
