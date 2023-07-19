e93e64f2ec8d067e6ce87d459411e033ade38b4e
Martin Abramovich <abramovich.martin@gmail.com>
2023-07-18 22:16:24 -0300

    index con for

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,8 +1,13 @@
-const controlador = {
-    index: (req, res) => {
+const fs = require('fs');
+const path = require('path');
+const productosPath = path.join(__dirname, "../data/productos.json");
 
-        res.render('index');
-    },
+const controlador = {
+    index: function (req, res) {
+        /* leo un json y lo parseo */
+        const productos = JSON.parse(fs.readFileSync(productosPath, 'utf-8'));
+        res.render("index", {productos});
+      },
 
     carrito: (req, res) => {
         res.render('products/carrito');
@@ -15,8 +20,6 @@ const controlador = {
     crear: (req, res) => {
         res.render('products/creacion');
     }
-
-
 }
 
 module.exports = controlador;
