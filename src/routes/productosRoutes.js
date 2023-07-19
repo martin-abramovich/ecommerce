5955154a46dd5aaa94063666eb31fd360210c098
Martin Abramovich <abramovich.martin@gmail.com>
2023-07-18 23:17:55 -0300

    editar

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,15 +1,20 @@
 const productosController = require('./../controllers/productosController')
-
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
 router.get('/', productosController.index);
 
 router.get('/carrito', productosController.carrito);
 
-router.get('/detalle', productosController.detalle);
+router.get('/detalle/:id', productosController.detalle);
 
 router.get('/crear', productosController.crear);
+
+/* form update */
+router.get("/edicion/:id", productosController.getUpdateForm);
+router.put("/edicion/:id", upload.single('imagen'), productosController.putUpdateForm);
 
 
 module.exports = router;
