18fcbe4233119761d1c579cf4e180816dcae76aa
Martin Abramovich <abramovich.martin@gmail.com>
2023-08-06 20:48:39 -0300

    validacion de registro

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,22 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-
 const registeredFilePath = path.join(__dirname, '../data/registrados.json');
-const 
-registered = JSON.parse(fs.readFileSync(registeredFilePath, 'utf-8'));
-
-
+const registered = JSON.parse(fs.readFileSync(registeredFilePath, 'utf-8'));
 const bcrypt = require('bcryptjs') // es lo que usaremos para incriptar la contraseña
+const {validationResult} = require('express-validator');
 
 //TODO: Encriptamos!!
 const encrypt = async (textPlain) => { 
     const hash = await bcrypt.hash(textPlain, 8) //es la cantidad de veces que se multiplicara (8)
     return hash//esto es lo que usaremos para llamar e incriptar un dato 
 }
-
-
-
-
 
 const controladorUsers = {
         
@@ -28,13 +21,26 @@ const controladorUsers = {
                 res.render("users/registro");
                 
         },
+
         create: async (req,res) =>{
-               let registrados = req.body
-               let idRegistrados= (registered[registered.length-1].id)+1
+               const resultValidation = validationResult(req);
+               if (resultValidation.errors.length > 0){
+                return res.render("users/registro", {
+                        errors: resultValidation.mapped(), //convierte array resultValidation en objeto literal
+                        oldData: req.body                        
+                })
+               };
+
+               let registrados = req.body;
+               let idRegistrados;
+               if (registered.length > 0){
+                idRegistrados = (registered[registered.length-1].id)+1;
+               } else {
+                idRegistrados = 1;
+               }
                
                
                const {Password} = req.body //creamos una constante con el dato o datos que quiera incriptar
-
                const Passwordhash = await encrypt(Password) //a esa constante la llamamos junto al hash y con (await encrypt(variable) la incriptamos)
                
 
@@ -43,9 +49,10 @@ const controladorUsers = {
                 Usuario: registrados.Usuario,
                 Apellido: registrados.Apellido,
                 Email: registrados.Email,
+                avatar: req.file ? `/img/avatars/${req.file.filename}`: "/img/default-game.jpg",
                 Password: Passwordhash
-
                }
+
                registered.push(ObjRegistrados);
                fs.writeFileSync(registeredFilePath, JSON.stringify(registered, null, ' '));
 
--- src/routes/usersRouters.js
+++ src/routes/usersRouters.js
@@ -1,10 +1,64 @@
 const usersController = require('./../controllers/usersController')
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const { body } = require("express-validator");
+const fs = require('fs');
+
+const usuariosPath = path.join(__dirname, '../data/registrados.json');
+function emailExists(Email) {
+    const usuarios = JSON.parse(fs.readFileSync(usuariosPath, 'utf8'));
+    return usuarios.some(usuario => usuario.Email === Email);
+  }
+
+const validations = [
+    body('Usuario').notEmpty().withMessage("El nombre no puede estar vacío"),
+    body('Apellido').notEmpty().withMessage("El apellido no puede estar vacío"),
+    body('Email').notEmpty().withMessage("El email no puede estar vacío").bail().isEmail().withMessage("Formato inválido"),
+    body('Password').notEmpty().withMessage("La contraseña no puede estar vacía"),
+    body('Password2').notEmpty().withMessage("Verificación de contraseña no puede estar vacía"),
+    body('avatar').custom((value, { req }) => {
+        let file = req.file;
+    
+        // Si no se proporciona un archivo, no hay nada que validar
+        if (!file) {
+            return true;
+        }
+    
+        let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+        let fileExtension = path.extname(file.originalname);
+    
+        if (!acceptedExtensions.includes(fileExtension)) {
+            throw new Error(`Formato de imagen inválido. Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+        }
+    
+        return true;
+    }),
+    body('Email').custom(async (value) => {
+        if (emailExists(value)) {
+          throw new Error('El correo electrónico ya está registrado');
+        }
+      })
+]
+
+/* configuración del almacenamiento de multer */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'../../public/img/avatars'));
+    },
+      filename: function (req, file, cb) {
+        console.log(path.extname(file.originalname))
+        const uniqueSuffix = Date.now();
+      cb(null, "product-" + uniqueSuffix + path.extname(file.originalname));
+    },
+  });
+
+const upload = multer({ storage: storage });
 
 router.get('/registro',usersController.registrarse);
-router.post('/registro', usersController.create)
+router.post('/registro', upload.single('avatar'), validations, usersController.create);
 
 router.get('/login',usersController.iniciarSesion);
 
