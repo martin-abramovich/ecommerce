fdba9e89adbc7de032d9b2fdcae196ee157d24a6
Martin Abramovich <abramovich.martin@gmail.com>
2023-08-11 17:32:47 -0300

    login solucionado

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const usersRouters = require('./src/routes/usersRouters');
 const methodOverride = require("method-override")
 const express = require("express");
 const path = require("path");
-const logMiddleware =require('./middlewares/logMiddleware');
-
+const logMiddleware = require('./middlewares/logMiddleware');
 
 const app = express();
 
@@ -28,8 +27,6 @@ app.use(logMiddleware);
 app.use('*', function(req, res) {
   res.send("Error de acceso, esta ruta no existe en el sitio")
 });
-
-app.use(session({secret:'guardar info'}));
 
 app.listen(4000, () => {
   console.log("Servidor corriendo en puerto 4000");
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,118 +1,100 @@
 const fs = require('fs');
 const path = require('path');
-const registeredFilePath = path.join(__dirname, '../data/registrados.json');
-const registered = JSON.parse(fs.readFileSync(registeredFilePath, 'utf-8'));
-const bcrypt = require('bcryptjs') // es lo que usaremos para incriptar la contraseña
-const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
-const jwt = require("jsonwebtoken")
-const users = require('../data/registrados.json')
+const jwt = require("jsonwebtoken");
+
+const registeredFilePath = path.join(__dirname, '../data/registrados.json');
 const usuariosPath = path.join(__dirname, '../data/registrados.json');
-const usuarios = JSON.parse(fs.readFileSync(usuariosPath, 'utf8'));
-const secretKey = 'Mi Llave Ultra Secreta'
-const token = jwt.sign(usuarios[0], secretKey)
+const secretKey = 'Mi Llave Ultra Secreta';
 
-cloudinary.config({ 
-        cloud_name: 'dgmxc8fal', 
-        api_key: '658928733417987', 
-        api_secret: 'CYEYZept3jVWxd829y_AsxNsp1E' 
-});
+const cloudinaryConfig = { 
+    cloud_name: 'dgmxc8fal', 
+    api_key: '658928733417987', 
+    api_secret: 'CYEYZept3jVWxd829y_AsxNsp1E' 
+};
+cloudinary.config(cloudinaryConfig);
 
-
-//TODO: Encriptamos!!
-const encrypt = async (textPlain) => { 
-    const hash = await bcrypt.hash(textPlain, 8) //es la cantidad de veces que se multiplicara (8)
-    return hash//esto es lo que usaremos para llamar e incriptar un dato 
-}
+const encrypt = async (textPlain) => {
+    const hash = await bcrypt.hash(textPlain, 8);
+    return hash;
+};
 
 const controladorUsers = {
-        
-        iniciarSesion: (req, res) => {
-                res.render("users/login");
-        },   
-
-        registrarse: (req, res) => {
-                res.render("users/registro");
-                
-        },
-
-        create: async (req,res) =>{
-               const resultValidation = validationResult(req);
-               if (resultValidation.errors.length > 0){
-                return res.render("users/registro", {
-                        errors: resultValidation.mapped(), //convierte array resultValidation en objeto literal
-                        oldData: req.body                        
-                })
-               };
-
-               let registrados = req.body;
-               let idRegistrados;
-               if (registered.length > 0){
-                idRegistrados = (registered[registered.length-1].id)+1;
-               } else {
-                idRegistrados = 1;
-               }
-               
-               const {Password} = req.body //creamos una constante con el dato o datos que quiera incriptar
-               const Passwordhash = await encrypt(Password) //a esa constante la llamamos junto al hash y con (await encrypt(variable) la incriptamos)
-
-               if (req.file) {
-                const imageBuffer = req.file.buffer;
-                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-                const customFilename = 'avatar' + uniqueSuffix;
-                const folder = 'ecommerce_dh/avatars';
-                
-                const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', folder: folder, public_id: customFilename }, (error, result) => {
-                 if (error) {
-                         console.error ('Error during upload: ', error);
-                 } else {
-                         console.log('Upload successful: ', result);
-                 }
-                });
- 
-                streamifier.createReadStream(imageBuffer).pipe(stream);
-               }
-               
-               let ObjRegistrados = {
-                id: idRegistrados,
-                Usuario: registrados.Usuario,
-                Apellido: registrados.Apellido,
-                Email: registrados.Email,
-                avatar: req.file ? `https://res.cloudinary.com/dgmxc8fal/image/upload/ecommerce_dh/avatars/${customFilename}`: "/img/default-avatar.png",
-                Password: Passwordhash
-               }
-
-               registered.push(ObjRegistrados);
-               fs.writeFileSync(registeredFilePath, JSON.stringify(registered, null, ' '));
-
-               res.redirect('/')
-        },
-        inicio: (req,res)=>{
-                 
-                const { Email, Password } = req.query;
-                const user = usuarios.find((i) => i.Email == Email && i.Password == Password);
-
-                
-
-                if (user) {
-
-                        const token = jwt.sign({
-                                exp: Math.floor(Date.now() / 1000) + 10,
-                                data: user,
-                            },
-                            secretKey
-                        );
-                
-                        res.send("ola")
-                           
-                    } else {
-                        res.send("Usuario o contraseña incorrecta");
-                    }
-                
+    iniciarSesion: (req, res) => {
+        res.render("users/login");
+    },
+    registrarse: (req, res) => {
+        res.render("users/registro");
+    },
+    create: async (req, res) => {
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render("users/registro", {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
         }
 
- }
-       
+        const registered = JSON.parse(fs.readFileSync(registeredFilePath, 'utf-8'));
+        const idRegistrados = registered.length > 0 ? registered[registered.length - 1].id + 1 : 1;
+
+        const { Password } = req.body;
+        const Passwordhash = await encrypt(Password);
+
+        let avatarUrl = "/img/default-avatar.png";
+        if (req.file) {
+            const imageBuffer = req.file.buffer;
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            const customFilename = 'avatar' + uniqueSuffix;
+            const folder = 'ecommerce_dh/avatars';
+
+            const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', folder: folder, public_id: customFilename }, (error, result) => {
+                if (error) {
+                    console.error('Error during upload: ', error);
+                } else {
+                    console.log('Upload successful: ', result);
+                }
+            });
+
+            streamifier.createReadStream(imageBuffer).pipe(stream);
+            avatarUrl = `https://res.cloudinary.com/dgmxc8fal/image/upload/ecommerce_dh/avatars/${customFilename}`;
+        }
+
+        const ObjRegistrados = {
+            id: idRegistrados,
+            Usuario: req.body.Usuario,
+            Apellido: req.body.Apellido,
+            Email: req.body.Email,
+            avatar: avatarUrl,
+            Password: Passwordhash
+        };
+
+        registered.push(ObjRegistrados);
+        fs.writeFileSync(registeredFilePath, JSON.stringify(registered, null, ' '));
+
+        res.redirect('/');
+    },
+    inicio: (req, res) => {
+        const usuarios = JSON.parse(fs.readFileSync(usuariosPath, 'utf8'));
+        const { email, password } = req.body;
+        const user = usuarios.find((i) => i.Email == email);
+    
+        if (user && bcrypt.compareSync(password, user.Password)) {
+            const token = jwt.sign({
+                    exp: Math.floor(Date.now() / 1000) + 10,
+                    data: user,
+                },
+                secretKey
+            );
+    
+            res.send("Sesión iniciada correctamente");
+        } else {
+            res.send("Usuario o contraseña incorrecta");
+        }
+    }
+};
 
 module.exports = controladorUsers;
