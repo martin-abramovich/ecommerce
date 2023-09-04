74ed8e5a1da5bd56e3c5c77704e2aba3db374f8a
Martin Abramovich <abramovich.martin@gmail.com>
2023-09-03 23:32:12 -0300

    correccion en el config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,17 +10,19 @@ module.exports = {
     "dialect": process.env.DB_DIALECT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
   }
 }
