9f431eade8a56a19d01f6dc6dff5d18b1dde6a2e
Martin Abramovich <abramovich.martin@gmail.com>
2023-09-03 23:20:46 -0300

    alwaysData

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "galante",
-    "host": "127.0.0.1",
-    "port": "3306",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
