99525f01d6d5650e5a0716170a39192dd19a7ce2
valentinascattini <132627938+valentinascattini@users.noreply.github.com>
2023-09-21 17:17:54 -0300

    probando validaciones front end

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,32 @@ window.addEventListener("load",function() {
     let formulario = document.querySelector("form.reservation");
 
     formulario.addEventListener("submit", function(e){
-        e.preventDefault();
+
+    let errores = [];
 
     let campoNombre = document.querySelector("input.name");
 
     if (campoNombre.value == ""){
-        alert("El campo del nombre tiene que estar completo");
+        errores.push("El campo del nombre tiene que estar completo");
+    } else if (campoNombre.value.length < 3){
+        errores.push ("El campo del nombre debe tener al menos 3 caracteres");
+    }
+
+    let campoContrasena = document.querySelector("input.password");
+
+    if (campoContrasena.value == ""){
+        errores.push("El campo de la contrasena tiene que estar completo");
+    } else if (campoContrasena.value.length < 8){
+        errores.push ("El campo del nombre debe tener al menos 8 caracteres");
+    }
+
+    if (errores.length > 0){
+        e.preventDefault();
+
+        let ulErrores = document.querySelector("div.errores ul")
+        for (let i=0; i<errores.length; i++)
+
+        ulErrores.innerHTML += "<li>" +  errores[i] + "</li>";
     }
     })
 })
