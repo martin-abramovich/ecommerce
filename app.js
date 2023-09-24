7d1cda727b4e1b48712179abdd234e7b93f4bd86
Martin Abramovich <abramovich.martin@gmail.com>
2023-09-24 19:20:11 -0300

    cambio session x cookie

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const session = require("express-session");
+const session = require("cookie-session");
 const path = require("path");
 const methodOverride = require("method-override");
 
