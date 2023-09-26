94cd854bafaf004fae52f696111a12ced25fe189
Daniel26jk <danielmoralese26@gmail.com>
2023-09-25 23:33:15 -0300

    cambio de port por error
    3300

--- middlewares/logMiddleware.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-function logMiddleware(req, res, next) {
-    fs.appendFileSync('log.txt', 'Se ingreso en la pagina ' + req.url);
-    next();
-}
-
-module.exports = logMiddleware;
